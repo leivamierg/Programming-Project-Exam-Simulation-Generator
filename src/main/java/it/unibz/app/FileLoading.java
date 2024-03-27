b3efc26aa4832b01e31ca422eeba02bc124c43ba
sebanardin <seba.tecnobar@gmail.com>
2024-03-27 18:43:35 +0100

    Interfaces modified

--- src/main/java/it/unibz/app/FileLoading.java
+++ src/main/java/it/unibz/app/FileLoading.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 
 public interface FileLoading {
     // methods
-    boolean loadFile(String fileName, String delimiter);
+    boolean loadFile(String fileName);
     // loads a file and returns false if it fails
-    void load(ArrayList<String> files, String delimiter);
+    void load(ArrayList<String> files);
     // loads all files in the bank package
 }
--- src/main/java/it/unibz/app/Question.java
+++ src/main/java/it/unibz/app/Question.java
@@ -3,15 +3,6 @@ package it.unibz.app;
 import java.util.ArrayList;
 
 public interface Question {
-    // attributes
-    String question = null;
-    ArrayList<String> answers = null;
-    // by convention, the first answer is the correct one
-    int appearencesNumber = 0;
-    // the more a question appears, lower is its probability to be chosen
-    boolean recentlyRightAnswered = false;
-    // if a question was rightly answered in the last sim, it won't be available for the next sim
-    // if it wasn't, it will be immediately asked in the next sim
 
     // methods
     String toString();
