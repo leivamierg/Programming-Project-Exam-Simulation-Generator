d541df06f096465ce2db9c1bad8d6347012e4917
sebanardin <seba.tecnobar@gmail.com>
2024-05-14 22:15:53 +0200

    FileLoaderTest completed

--- src/main/java/it/unibz/model/FileLoaderInt.java
+++ src/main/java/it/unibz/model/FileLoaderInt.java
@@ -8,7 +8,7 @@ public interface FileLoaderInt {
      * loads all files in the given bank
      * @param bankName path to the bank you want to load files from
      */
-    public List<Topic> loadBank(String bankName);
+    List<Topic> loadBank(String bankName);
     /**
      * loads an input bank file -> transforms the input file into a Topic object
      * @param fileName path to the bank file you want to load
